0a2cc26fd63f3a75d95c5e0596032147732d335c
CaioAReis <Caio.almeida16.ca@gmail.com>
2021-07-11 13:38:34 -0300

    Iniciando API - Conexão com o banco de dados MySQL e Refatoração do DER

---
+++ VesteMeAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VesteMeAPI.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        //  Testando o controller
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello World, Web API with ASP.Net Core.";
+        }
+    }
+}
---
+++ VesteMeAPI/Data/AplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VesteMeAPI.Data
+{
+    public class AplicationDBContext : DbContext 
+    {
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options) { }
+    }
+}
